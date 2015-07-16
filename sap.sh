11f358b4bf9351c338b498dd56fa86dbb2826c03
Yashar Ahmedizade <yashade2001@gmail.com>
2015-07-16 13:44:28 +0300

    less hardcoding

--- sap.sh
+++ sap.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 set -e
 
+###################################
+#       SET VARIABLES FIRST       #
+###################################
+
+# your username (in your server)
 SERVER_USERNAME="yashar"
-SERVER_HOSTNAME="example.org"
+# server's hostname
+SERVER_HOSTNAME="example.net"
+# port number (usually 22)
 SERVER_PORT="22"
-ROM_SOURCE="~/aosp/"
-SYSTEMUI_OUT=$ROM_SOURCE"out/target/product/hammerhead/system/priv-app/SystemUI/SystemUI.apk"
-SETTINGS_OUT=$ROM_SOURCE"out/target/product/hammerhead/system/priv-app/Settings/Settings.apk"
-SETTINGSPROVIDER_OUT=$ROM_SOURCE"out/target/product/hammerhead/system/priv-app/SettingsProvider/SettingsProvider.apk"
-FRAMEWORK_OUT=$ROM_SOURCE"out/target/product/hammerhead/system/framework/framework.jar"
-FRAMEWORKRES_OUT=$ROM_SOURCE"out/target/product/hammerhead/system/framework/framework-res.apk"
+# path to android source tree
+ROM_SOURCE="~/aosp-5.1/"
+# device codename
+DEVICE="shamu"
+# "downloads" directory for this script (if you set OUT_FILES_DIR="cats" files will be downloaded to /home/$USER/cats/)
 OUT_FILES_DIR="outfiles"
+
+
+# NO NEED TO EDIT THESE
+SYSTEMUI_OUT=$ROM_SOURCE"out/target/product/$DEVICE/system/priv-app/SystemUI/SystemUI.apk"
+SETTINGS_OUT=$ROM_SOURCE"out/target/product/$DEVICE/system/priv-app/Settings/Settings.apk"
+SETTINGSPROVIDER_OUT=$ROM_SOURCE"out/target/product/$DEVICE/system/priv-app/SettingsProvider/SettingsProvider.apk"
+FRAMEWORK_OUT=$ROM_SOURCE"out/target/product/$DEVICE/system/framework/framework.jar"
+FRAMEWORKRES_OUT=$ROM_SOURCE"out/target/product/$DEVICE/system/framework/framework-res.apk"
 
 rm -rf /home/$USER/$OUT_FILES_DIR
 mkdir /home/$USER/$OUT_FILES_DIR
@@ -18,6 +32,7 @@ mkdir /home/$USER/$OUT_FILES_DIR
 scp -P $SERVER_PORT $SERVER_USERNAME@$SERVER_HOSTNAME:"$SYSTEMUI_OUT $SETTINGS_OUT $SETTINGSPROVIDER_OUT $FRAMEWORK_OUT $FRAMEWORKRES_OUT" /home/$USER/$OUT_FILES_DIR
 
 adb start-server
+adb root
 adb remount
 
 adb push /home/$USER/$OUT_FILES_DIR/SystemUI.apk /system/priv-app/SystemUI/SystemUI.apk
